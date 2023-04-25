5db9ab279caf3d90f95bd15617577cd569f8bb45
gBogdan95 <bogdan.ghinga@gmail.com>
2023-04-25 17:37:10 +0300

    finalizing app

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useContext } from "react";
 import Messages from './Messages';
 import Input from './Input';
 import { auth } from "../firebase"
 import { signOut } from "firebase/auth"
+import { ChatContext } from "../context/ChatContext";
 
 function Chat() {
+    const { data } = useContext(ChatContext);
+
     return (
         <div className='chat'>
             <div className="chatInfo">
-                <span>Jane</span>
+            <span>{data.user?.displayName}</span>
                 <div className="logout">
                 <button onClick={()=>signOut(auth)}>Logout</button>
                 </div>
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { useContext, useEffect, useRef } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
 
-function Message() {
-    return (
-        <div className='message owner'>
-            <div className="messageInfo">
-                <img src="https://images.squarespace-cdn.com/content/v1/55db3b44e4b01e5be14cad77/1657893813325-56Q5M5A6GELA24XFRKUU/Screenshot+2022-07-15+at+16.03.18.png?format=1500w" alt="" />
-                <span>Just now</span>
-            </div>
-            <div className="messageContent">
-                <p>hello</p>
-                <img src="https://images.squarespace-cdn.com/content/v1/55db3b44e4b01e5be14cad77/1657893813325-56Q5M5A6GELA24XFRKUU/Screenshot+2022-07-15+at+16.03.18.png?format=1500w" alt="" />
-            </div>
-        </div>
-    );
-}
+const Message = ({ message }) => {
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
+
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  }, [message]);
+
+  return (
+    <div
+      ref={ref}
+      className={`message ${message.senderId === currentUser.uid && "owner"}`}
+    >
+      <div className="messageInfo">
+        <img
+          src={
+            message.senderId === currentUser.uid
+              ? currentUser.photoURL
+              : data.user.photoURL
+          }
+          alt=""
+        />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>{message.text}</p>
+        {message.img && <img src={message.img} alt="" />}
+      </div>
+    </div>
+  );
+};
 
 export default Message;
